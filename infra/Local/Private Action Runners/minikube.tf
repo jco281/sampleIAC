2d491b91a167b5bdfb843ed4a8f6fc6ed6027186
Jeff Overton <jco281@me.com>
2024-07-18 16:05:20 -0600

    Add DinD to ARC runner set

--- infra/Local/Private Action Runners/minikube.tf
+++ infra/Local/Private Action Runners/minikube.tf
@@ -84,13 +84,199 @@ resource "helm_release" "runner_scale_set" {
 
   set {
     name  = "minRunners"
-    value = 1
+    value = "1"
   }
 
   set {
     name  = "maxRunners"
-    value = 5
+    value = "5"
   }
+
+  set {
+    name  = "template.spec.initContainers[0].name"
+    value = "init-dind-externals"
+  }
+
+  set {
+    name  = "template.spec.initContainers[0].image"
+    value = "ghcr.io/actions/actions-runner:latest"
+  }
+
+  set {
+    name  = "template.spec.initContainers[0].command[0]"
+    value = "cp"
+  }
+
+  set {
+    name  = "template.spec.initContainers[0].command[1]"
+    value = "-r"
+  }
+
+  set {
+    name  = "template.spec.initContainers[0].command[2]"
+    value = "-v"
+  }
+
+  set {
+    name  = "template.spec.initContainers[0].command[3]"
+    value = "/home/runner/externals/."
+  }
+
+  set {
+    name  = "template.spec.initContainers[0].command[4]"
+    value = "/home/runner/tmpDir/"
+  }
+
+  set {
+    name  = "template.spec.initContainers[0].volumeMounts[0].name"
+    value = "dind-externals"
+  }
+
+  set {
+    name  = "template.spec.initContainers[0].volumeMounts[0].mountPath"
+    value = "/home/runner/tmpDir"
+  }
+
+  set {
+    name  = "template.spec.containers[0].name"
+    value = "runner"
+  }
+
+  set {
+    name  = "template.spec.containers[0].image"
+    value = "ghcr.io/actions/actions-runner:latest"
+  }
+
+  set {
+    name  = "template.spec.containers[0].command[0]"
+    value = "/home/runner/run.sh"
+  }
+
+  set {
+    name  = "template.spec.containers[0].env[0].name"
+    value = "DOCKER_HOST"
+  }
+
+  set {
+    name  = "template.spec.containers[0].env[0].value"
+    value = "unix:///var/run/docker.sock"
+  }
+
+  set {
+    name  = "template.spec.containers[0].volumeMounts[0].name"
+    value = "work"
+  }
+
+  set {
+    name  = "template.spec.containers[0].volumeMounts[0].mountPath"
+    value = "/home/runner/_work"
+  }
+
+  set {
+    name  = "template.spec.containers[0].volumeMounts[1].name"
+    value = "dind-sock"
+  }
+
+  set {
+    name  = "template.spec.containers[0].volumeMounts[1].mountPath"
+    value = "/var/run"
+  }
+
+  set {
+    name  = "template.spec.containers[1].name"
+    value = "dind"
+  }
+
+  set {
+    name  = "template.spec.containers[1].image"
+    value = "docker:dind"
+  }
+
+  set {
+    name  = "template.spec.containers[1].args[0]"
+    value = "dockerd"
+  }
+
+  set {
+    name  = "template.spec.containers[1].args[1]"
+    value = "--host=unix:///var/run/docker.sock"
+  }
+
+  set {
+    name  = "template.spec.containers[1].args[2]"
+    value = "--group=$(DOCKER_GROUP_GID)"
+  }
+
+  set {
+    name  = "template.spec.containers[1].env[0].name"
+    value = "DOCKER_GROUP_GID"
+  }
+
+  set {
+    name  = "template.spec.containers[1].securityContext.privileged"
+    value = "true"
+  }
+
+  set {
+    name  = "template.spec.containers[1].volumeMounts[0].name"
+    value = "work"
+  }
+
+  set {
+    name  = "template.spec.containers[1].volumeMounts[0].mountPath"
+    value = "/home/runner/_work"
+  }
+
+  set {
+    name  = "template.spec.containers[1].volumeMounts[1].name"
+    value = "dind-sock"
+  }
+
+  set {
+    name  = "template.spec.containers[1].volumeMounts[1].mountPath"
+    value = "/var/run"
+  }
+
+  set {
+    name  = "template.spec.containers[1].volumeMounts[2].name"
+    value = "dind-externals"
+  }
+
+  set {
+    name  = "template.spec.containers[1].volumeMounts[2].mountPath"
+    value = "/home/runner/externals"
+  }
+
+  set {
+    name  = "template.spec.volumes[0].name"
+    value = "work"
+  }
+
+  set {
+    name  = "template.spec.volumes[0].emptyDir.medium"
+    value = ""
+  }
+
+  set {
+    name  = "template.spec.volumes[1].name"
+    value = "dind-sock"
+  }
+
+  set {
+    name  = "template.spec.volumes[1].emptyDir.medium"
+    value = ""
+  }
+
+  set {
+    name  = "template.spec.volumes[2].name"
+    value = "dind-externals"
+  }
+
+  set {
+    name  = "template.spec.volumes[2].emptyDir.medium"
+    value = ""
+  }
+
 
 }
 
